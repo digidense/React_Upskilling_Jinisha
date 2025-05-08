d0aa0a86d80e6916a9858247b67c4cd6f64dc4e2
jinisha-a-s <jinisha.agustinraj@digidense.in>
2025-05-08 15:11:22 +0530

    UseContext

--- myreactProject/src/App.jsx
+++ myreactProject/src/App.jsx
@@ -2,14 +2,25 @@ import Hookuseeffect from "./Hookuseeffect";
 import Hookusestate from "./Hookusestate";
 import FocusInput from './Hookuseref';
 import UseMemoExample from './Hookusememo';
-function App(){
+import Container from './HookUseContext/Container';
+import React, { createContext, use, useState } from "react";
+
+export const ThemeContext = createContext();
+
+function App() {
     
+    const[theme, setTheme]=useState('light')
+
     return <>
-     {/* <Hookusestate/> */}
-     {/* <Hookuseeffect/> */}
-     {/* <Hookusestate/> */}
-     {/* <FocusInput/> */}
-     <UseMemoExample/>
+        {/* <Hookusestate/> */}
+        {/* <Hookuseeffect/> */}
+        {/* <Hookusestate/> */}
+        {/* <FocusInput/> */}
+        {/* <UseMemoExample/> */}
+        <ThemeContext.Provider value={{theme, setTheme}}>
+            <Container/>
+        </ThemeContext.Provider>
+       
     </>
 }
 
---
+++ myreactProject/src/HookUseContext/Container.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { useContext } from 'react'
+import Users from './Users'
+
+const Container = () => {
+  return (
+    <div>
+        <Users/>
+    </div>
+    
+  )
+}
+
+export default Container
